7836e522771bde1c54edcf63450279a5e8256f90
igornepipenko <igornepipenko@gmail.com>
2017-09-26 15:09:01 +0300

    first commit

--- docker/frontend/docker-entrypoint.sh
+++ docker/frontend/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cd /var/www/planarsys
+cd /var/www/angular-course
 
-if [ ! -d /var/www/planarsys/node_modules ]; then
+if [ ! -d /var/www/angular-course/node_modules ]; then
   npm cache clean -f  &&  npm install
 fi;
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,36 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    })
+      .compileComponents();
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+    // tslint:disable-next-line
     const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app)
+      .toBeTruthy();
   }));
   it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+    // tslint:disable-next-line
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title)
+      .toEqual('app');
   }));
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
+    // tslint:disable-next-line
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent)
+      .toContain('Welcome to app!');
   }));
 });
